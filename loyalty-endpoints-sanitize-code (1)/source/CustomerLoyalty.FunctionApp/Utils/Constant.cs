bfc2f90a28e57eb0b4a9e4e36fd9963806dc6cb5
harsh12sp <harsh12sp@gmail.com>
2024-12-06 22:07:36 +0000

    Delete loyalty-endpoints-sanitize-code (1) directory

--- loyalty-endpoints-sanitize-code (1)/source/CustomerLoyalty.FunctionApp/Utils/Constant.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace BenjaminMoore.Api.Retail.Pos.CustomerLoyalty.FunctionApp.Utils
-{
-    public static class Constant
-    {
-        public const string CreateCustomerLoyaltyFunctionName = "CreateCustomerLoyalty";
-    }
-}
